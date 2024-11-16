dd43cbcd4c8573b6e9e738305bf1512f845e0886
MebratuAG <mebratuantoniyos@gmail.com>
2024-11-16 19:52:27 +0100

    Netflix Header and Footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>This is Netflix-clone</h1>
+      <Home />
     </div>
   );
 }
