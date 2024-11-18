84d7b501c6773b57a8afc7167f70908258829dcc
MebratuAG <mebratuantoniyos@gmail.com>
2024-11-18 18:46:14 +0100

    Banner

--- netflix-clone/src/Pages/Home/Home.jsx
+++ netflix-clone/src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../components/Header/Header.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
+import Banner from "../../components/Banner/Banner.jsx";
 function Home() {
   return (
     <>
       <Header />
+      <Banner />
       <Footer />
     </>
   );
