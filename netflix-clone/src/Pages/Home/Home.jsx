440ccf25b9d52b980ec696fae5e21649fb2c4cff
MebratuAG <mebratuantoniyos@gmail.com>
2024-11-20 18:30:59 +0100

    Rows Added

--- netflix-clone/src/Pages/Home/Home.jsx
+++ netflix-clone/src/Pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "../../components/Header/Header.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
 import Banner from "../../components/Banner/Banner.jsx";
+import RowList from "../../components/Rows/RowList/RowList.jsx";
 function Home() {
   return (
     <>
       <Header />
       <Banner />
+      <RowList />
       <Footer />
     </>
   );
